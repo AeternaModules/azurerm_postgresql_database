8ee7f7149f8ad28e303a86cd758aa0aa433d630d
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "postgresql_databases_id" {
   description = "Map of id values across all postgresql_databases, keyed the same as var.postgresql_databases"
-  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.id }
+  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "postgresql_databases_charset" {
   description = "Map of charset values across all postgresql_databases, keyed the same as var.postgresql_databases"
-  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.charset }
+  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.charset if v.charset != null && length(v.charset) > 0 }
 }
 output "postgresql_databases_collation" {
   description = "Map of collation values across all postgresql_databases, keyed the same as var.postgresql_databases"
-  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.collation }
+  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.collation if v.collation != null && length(v.collation) > 0 }
 }
 output "postgresql_databases_name" {
   description = "Map of name values across all postgresql_databases, keyed the same as var.postgresql_databases"
-  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.name }
+  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "postgresql_databases_resource_group_name" {
   description = "Map of resource_group_name values across all postgresql_databases, keyed the same as var.postgresql_databases"
-  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.resource_group_name }
+  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "postgresql_databases_server_name" {
   description = "Map of server_name values across all postgresql_databases, keyed the same as var.postgresql_databases"
-  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.server_name }
+  value       = { for k, v in azurerm_postgresql_database.postgresql_databases : k => v.server_name if v.server_name != null && length(v.server_name) > 0 }
 }
 
